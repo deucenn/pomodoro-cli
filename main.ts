e47cea5e30bf2744371cdaee872a1c843e6a25cb
deucenn <panix.black@web.de>
2024-12-02 15:52:00 +0100

    Imported readline for inputs

---
+++ main.ts
@@ -0,0 +1,17 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const Pomodoro = (workTime: number, breakTime: number) => {
+
+}
+
+rl.question("How long should the work time be in minutes?", (workTime) => {
+    rl.question("How long should the break time be in minutes?", (breakTime) => {
+
+    },
+}
+)
