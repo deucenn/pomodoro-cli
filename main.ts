b0ce48661519ae788713bb21bfd5cd0f1693badb
deucenn <panix.black@web.de>
2024-12-03 17:51:47 +0100

    Added colors

--- main.ts
+++ main.ts
@@ -2,13 +2,14 @@
 import inquirer from "inquirer";
 import notifier from "node-notifier";
 import { Bar, Presets } from "cli-progress";
+import "colors";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const Pomodoro = async (workTime: number, breakTime: number) => {
   console.clear();
 
-  console.log(`💻 Starting a ${workTime}-minute work timer!`);
+  console.log(`💻 Starting a ${workTime}-minute work timer!`.blue);
   notifier.notify({
     title: "Pomodoro Timer",
     message: "💻Work time started!💻",
@@ -19,7 +20,7 @@ const Pomodoro = async (workTime: number, breakTime: number) => {
   console.clear();
   const workBar = new Bar(
     {
-      format: '💻 Work Time |{bar}| {value}/{total} minutes left',
+      format: '💻 Work Time |{bar}| {value}/{total} minutes left'.blue,
     },
     Presets.shades_classic
   );
@@ -31,10 +32,10 @@ const Pomodoro = async (workTime: number, breakTime: number) => {
   }
   workBar.stop();
 
-  console.log("☕ Work time is up! Time for a break!");
+  console.log("☕ Work time is up! Time for a break!".magenta);
   await sleep(1000);
 
-  console.log(`☕ Starting a ${breakTime}-minute work timer!`);
+  console.log(`☕ Starting a ${breakTime}-minute work timer!`.green);
   notifier.notify({
     title: "Pomodoro Timer",
     message: "☕Break time started!☕",
@@ -45,7 +46,7 @@ const Pomodoro = async (workTime: number, breakTime: number) => {
   console.clear();
   const breakBar = new Bar(
     {
-      format: '💤 Break Time |{bar}| {value}/{total} minutes left',
+      format: '💤 Break Time |{bar}| {value}/{total} minutes left'.green,
     },
     Presets.shades_classic
   );
@@ -56,7 +57,7 @@ const Pomodoro = async (workTime: number, breakTime: number) => {
     breakBar.update(i + 1);
   }
   breakBar.stop();
-  console.log("Break time is over!");
+  console.log("Break time is over!".magenta);
 
   await sleep(1000);
 
@@ -64,12 +65,12 @@ const Pomodoro = async (workTime: number, breakTime: number) => {
     {
       type: "list",
       name: "continueSession",
-      message: "Do you want to continue the session?",
-      choices: ["Yes", "No"],
+      message: "Do you want to continue the session?".magenta,
+      choices: ["Yes".blue, "No".green],
     },
   ]);
 
-  if (continueSession === "Yes") {
+  if (continueSession === "Yes".blue) {
     console.log("Continuing the session!");
     await sleep(1000);
     await Pomodoro(workTime, breakTime);
@@ -87,27 +88,27 @@ const main = async () => {
    | |_) / _ \\| '_ \` _ \\ / _ \\| '__/ _\` |/ _ \\    | | | | '_ \` _ \\ / _ \\ '__|
    |  __/ (_) | | | | | | (_) | | | (_| | (_) |   | | | | | | | | |  __/ |   
    |_|   \\___/|_| |_| |_|\\___/|_|  \\__,_|\\___/    |_| |_|_| |_| |_|\\___|_|    
-   `);
+   `.blue);
 
   const { menuChoice } = await inquirer.prompt([
     {
       type: "list",
       name: "menuChoice",
       message: "Welcome to my Pomodoro Timer",
-      choices: ["Start", "Settings", "Exit"],
+      choices: ["Start".blue, "Settings".green, "Exit".red],
     },
   ]);
 
-  if (menuChoice === "Start") {
+  if (menuChoice === "Start".blue) {
     console.log("Starting the session!");
     await Pomodoro(25, 5);
-  } else if (menuChoice === "Settings") {
+  } else if (menuChoice === "Settings".green) {
     const answers = await inquirer.prompt([
       {
         type: "input",
         name: "workTime",
         message: "How long should the work time be in minutes?",
-        default: "25",
+        default: "25".grey,
         validate: (input: string) =>
           parseInt(input, 10) > 0
             ? true
@@ -117,7 +118,7 @@ const main = async () => {
         type: "input",
         name: "breakTime",
         message: "How long should the break time be in minutes?",
-        default: "5",
+        default: "5".grey,
         validate: (input: string) =>
           parseInt(input, 10) > 0
             ? true
@@ -129,9 +130,10 @@ const main = async () => {
     const breakTimer = parseInt(answers.breakTime, 10);
 
     await Pomodoro(workTimer, breakTimer);
-  } else if (menuChoice === "Exit") {
+  } else if (menuChoice === "Exit".red) {
     console.clear();
-    console.log("Thank you for using the Pomodoro timer! Goodbye!");
+    console.log("Thank you for using the Pomodoro timer! Goodbye!".red.dim);
+    await sleep(1000);
   }
 };
 
